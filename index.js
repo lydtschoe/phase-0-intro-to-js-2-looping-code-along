4816421d00e290c7371f5027810f822c423c36df
lydia tschoe <lydiatschoe@gmail.com>
2023-06-08 13:45:27 -0400

    package-lock.json

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+function writeCards(names, eventName) {
+    const thankYouCards = [];
+  
+    for (let i = 0; i < names.length; i++) {
+      const thankYouMessage = `Thank you, ${names[i]}, for the wonderful ${eventName} gift!`;
+      thankYouCards.push(thankYouMessage);
+    }
+  
+    return thankYouCards;
+  }
+  function countDown(number) {
+    while (number >= 0) {
+      console.log(number);
+      number--;
+    }
+  }
